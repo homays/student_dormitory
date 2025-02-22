f6824ed0a107ed6f9c8eb55f28a1cf336befc8dd
ymkx <2362552845@qq.com>
2025-02-22 20:44:51 +0800

    feat: 首页新增统计图

--- vue/src/views/manager/Home.vue
+++ vue/src/views/manager/Home.vue
@@ -5,10 +5,10 @@
     </div>
 
     <div style="display: flex; margin: 10px 0">
-      <div style="width: 50%;" class="card">
+      <div style="width: 100%;" class="card">
         <div style="margin-bottom: 30px; font-size: 20px; font-weight: bold">公告列表</div>
-        <div >
-          <el-timeline  reverse slot="reference">
+        <div>
+          <el-timeline reverse slot="reference">
             <el-timeline-item v-for="item in notices" :key="item.id" :timestamp="item.time">
               <el-popover
                   placement="right"
@@ -22,11 +22,70 @@
         </div>
       </div>
     </div>
+    <div style="display: flex" v-if="user.role ==='ADMIN'">
+      <div style="width: 50%; height: 400px" id="pie1" class="card"></div>
+      <div style="width: 50%; height: 400px" id="pie2" class="card"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import * as echarts from "echarts"
 
+let pie1Options = {
+  title: {
+    text: '电费缴纳情况统计', // 主标题
+    subtext: '统计维度: 缴纳状态', // 副标题
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item',
+    formatter: '{a} <br/>{b} : {c} ({d}%)'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: '', // 鼠标移上去显示内容
+      type: 'pie',
+      radius: '50%',
+      center: ['50%', '60%'],
+      data: [
+        {value: 100, name: '已缴费'},
+        {value: 100, name: '未缴费'},
+      ]
+    }
+  ]
+}
+let pie2Options = {
+  title: {
+    text: '宿舍住宿情况统计', // 主标题
+    subtext: '统计维度: 是否住满', // 副标题
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item',
+    formatter: '{a} <br/>{b} : {c} ({d}%)'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: '', // 鼠标移上去显示内容
+      type: 'pie',
+      radius: '50%',
+      center: ['50%', '60%'],
+      data: [
+        {value: 100, name: '已住满'},
+        {value: 100, name: '未住满'},
+      ]
+    }
+  ]
+}
 export default {
   name: 'Home',
   data() {
@@ -39,6 +98,30 @@ export default {
     this.$request.get('/notice/selectAll').then(res => {
       this.notices = res.data || []
     })
+    this.loadElectro()
+    this.loadStay()
+  },
+  methods: {
+    loadElectro() {
+      this.$request.get('/echarts/electro').then(res => {
+        if (res.code === '200') {
+          let chartDom = document.getElementById('pie1');
+          let myChart = echarts.init(chartDom);
+          pie1Options.series[0].data = res.data
+          myChart.setOption(pie1Options)
+        }
+      })
+    },
+    loadStay() {
+      this.$request.get('/echarts/stay').then(res => {
+        if (res.code === '200') {
+          let chartDom = document.getElementById('pie2');
+          let myChart = echarts.init(chartDom);
+          pie2Options.series[0].data = res.data
+          myChart.setOption(pie2Options)
+        }
+      })
+    }
   }
 }
 </script>
